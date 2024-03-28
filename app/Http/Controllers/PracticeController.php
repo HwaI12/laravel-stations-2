55dcaaa27c64edede0a2e58e54ab39d496597441
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-03-28 10:55:07 +0900

    add-PHP03

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,12 +6,12 @@ class PracticeController extends Controller
 {
     public function sample()
     {
-        return response('practice');
+        return view('practice');
     }
 
-          public function sample2()
+    public function sample2()
     {
         $test = 'practice2';
-                    return response($test);
+            return view('practice2', ['testParam' => $test]);
     }
 }
